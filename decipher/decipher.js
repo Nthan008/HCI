963f70e36faadb6bda423dc0f28ef8fecc203d6f
Nthan008 <114546358+Nthan008@users.noreply.github.com>
2022-12-05 19:37:51 +0700

    Add files via upload

--- decipher/decipher.js
+++ decipher/decipher.js
@@ -1,15 +1,13 @@
 function digitalDecipher(mes, key){
 	for(let i = 0; i < mes.length; i++){
-	    mes[i] -= parseInt(key.toString()[i % key.toString().length]);
-    }
+	    mes[i] -= parseInt(key.toString()[i % key.toString().length]);}
 	const letters = "abcdefghijklmnopqrstuvwxyz";
 	var res = "";
 	for(let i = 0; i < mes.length; i++){
-		res += letters[mes[i] - 1];
-	}
+		res += letters[mes[i] - 1];}
 	return res;
 }
 
-console.log(digitalDecipher([20, 12, 18, 30, 21], 1939))
-console.log(digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990))
-console.log(digitalDecipher([6, 4, 1, 3, 9, 20], 100))
+console.log(digitalDecipher([10, 12, 17, 24, 11, 10, 23, 16, 23, 5, 20] , 7023)) // -> cloudjumper
+console.log(digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990)) //-> mubashir
+console.log(digitalDecipher([10, 4, 1, 3, 9, 20], 100)) // -> idabit
